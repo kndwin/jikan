2e78cd08eb67cfefdf187a9e8cdedc0b502c0bf6
Kevin Nguyen <me@kndwin.dev>
2023-07-02 14:36:22 +1000

    fix: update base url

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,15 +1,12 @@
 import { initClient } from "@ts-rest/core";
 import { initQueryClient } from "@ts-rest/react-query";
 
-export { useTsRestQueryClient } from "@ts-rest/react-query";
 import { contract } from "./contract";
 
-export const client = initClient(contract, {
-  baseUrl: "http://localhost:3000/api",
+const clientConfig = {
+  baseUrl: `${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
   baseHeaders: {},
-});
+};
 
-export const queryClient = initQueryClient(contract, {
-  baseUrl: "http://localhost:3000/api",
-  baseHeaders: {},
-});
+export const client = initClient(contract, clientConfig);
+export const queryClient = initQueryClient(contract, clientConfig);
