045f85c73ebe9a1654ac3532f054a5c51be277f5
kndwin <tnkevino@gmail.com>
2023-07-04 11:30:25 +1000

    fix: remove edge function

--- src/pages/api/[...ts-rest].tsx
+++ src/pages/api/[...ts-rest].tsx
@@ -3,10 +3,4 @@ import { createNextRouter } from "@ts-rest/next";
 import { contract } from "@/api/contract";
 import { router } from "@/api/router";
 
-export const config = {
-  runtime: "edge",
-  regions: ["syd1"],
-  unstable_allowDynamic: ["node_modules/better-sqlite3/**"],
-};
-
 export default createNextRouter(contract, router);
