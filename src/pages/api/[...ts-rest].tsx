93264a3dee214871ec2f1b6d98e1fb1f65e89598
kndwin <tnkevino@gmail.com>
2023-07-04 10:59:31 +1000

    feat: added edge runtime

--- src/pages/api/[...ts-rest].tsx
+++ src/pages/api/[...ts-rest].tsx
@@ -1,5 +1,11 @@
 import { createNextRouter } from "@ts-rest/next";
+
 import { contract } from "@/api/contract";
 import { router } from "@/api/router";
+
+export const config = {
+  runtime: "edge",
+  regions: ["syd1"],
+};
 
 export default createNextRouter(contract, router);
